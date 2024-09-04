fae43f8dad08a3faf5b5473bef01aad282799346
khadija zekraoui <khadija.zekraoui.externe@macif.fr>
2024-09-04 17:53:37 +0200

    practice - understand logical AND operator

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -9,5 +9,15 @@ public class Hello {
             System.out.println("And I am scared of aliens");
         }
 
+        int topScore = 80;
+        if (topScore < 100) {
+            System.out.println("You got the high score!");
+        }
+
+        int secondTopScore = 81;
+        if (topScore > secondTopScore && topScore < 100) {
+            System.out.println("Greater than second top score and less than 100");
+        }
+
     }
 }

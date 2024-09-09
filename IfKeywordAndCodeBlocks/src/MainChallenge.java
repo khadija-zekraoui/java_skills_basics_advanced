f39602b3bb279f41dc4e58cb4d953bb8e82105e0
khadija zekraoui <khadija.zekraoui.externe@macif.fr>
2024-09-09 10:42:26 +0200

    parctice - if then else Challenge

---
+++ IfKeywordAndCodeBlocks/src/MainChallenge.java
@@ -0,0 +1,13 @@
+public class MainChallenge {
+
+    public static void main(String[] args) {
+
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        int finalScore = score;
+
+    }
+}

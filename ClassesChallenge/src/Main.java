a5aa035e973b3aba7af3ba9e1ad0602b04c06385
khadija zekraoui <khadija.zekraoui.externe@macif.fr>
2024-09-19 13:33:23 +0200

    practice - constructor chaining

--- ClassesChallenge/src/Main.java
+++ ClassesChallenge/src/Main.java
@@ -2,19 +2,21 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Account miasAccount = new Account("12345", 500,
-                "Mia Luna", "myemail@mia.com",
-                "(087) 123-4567");
+//        Account miasAccount = new Account("12345", 500,
+//                "Mia luna", "myemail@mia.com",
+//                "(087) 123-4567");
+
+        Account miasAccount = new Account();
 
         System.out.println(miasAccount.getNumber());
         System.out.println(miasAccount.getBalance());
 
-//        bobsAccount.setNumber("12345");
-//        bobsAccount.setBalance(1000.00);
-//        bobsAccount.setCustomerName("Mia Luna");
-//        bobsAccount.setCustomerEmail("myemail@bob.com");
-//        bobsAccount.setCustomerPhone("(087) 123-4567");
-
+//        miasAccount.setNumber("12345");
+//        miasAccount.setBalance(1000.00);
+//        miasAccount.setCustomerName("Mia Luna");
+//        miasAccount.setCustomerEmail("myemail@mia.com");
+//        miasAccount.setCustomerPhone("(087) 123-4567");
+        
         miasAccount.withdrawFunds(100.0);
         miasAccount.depositFunds(250);
         miasAccount.withdrawFunds(50);
@@ -26,5 +28,10 @@ public class Main {
         miasAccount.withdrawFunds(54.46);
 
         miasAccount.withdrawFunds(54.45);
+
+        Account olafsAccount = new Account("mia",
+                "olaf@email.com", "12345");
+        System.out.println("AccountNo: " + olafsAccount.getNumber() +
+                "; name " + olafsAccount.getCustomerName());
     }
 }

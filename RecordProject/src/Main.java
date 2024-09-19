4645722418737d720c4c277b4b266f95e0c49a63
khadija zekraoui <khadija.zekraoui.externe@macif.fr>
2024-09-19 17:47:27 +0200

    practice - create a POJO

--- RecordProject/src/Main.java
+++ RecordProject/src/Main.java
@@ -5,14 +5,14 @@ public class Main {
         for (int i = 1; i <= 5; i++) {
             Student s = new Student("S92300" + i,
                     switch (i) {
-                        case 1 -> "Mary";
-                        case 2 -> "Carol";
-                        case 3 -> "Tim";
-                        case 4 -> "Harry";
+                        case 1 -> "Mia";
+                        case 2 -> "olaf";
+                        case 3 -> "bee";
+                        case 4 -> "Luna";
                         case 5 -> "Lisa";
                         default -> "Anonymous";
                     },
-                    "05/11/1985",
+                    "05/11/2022",
                     "Java Masterclass");
             System.out.println(s);
         }

2a2e86d43b6fa687ef039ebefbbdcab1ce4649c3
khadija zekraoui <khadija.zekraoui.externe@macif.fr>
2024-09-19 18:03:36 +0200

    practice - Create a record

--- RecordProject/src/Main.java
+++ RecordProject/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
 
         for (int i = 1; i <= 5; i++) {
-            Student s = new Student("S92300" + i,
+            LPAStudent s = new LPAStudent("S92300" + i,
                     switch (i) {
                         case 1 -> "Mia";
                         case 2 -> "olaf";
@@ -16,5 +16,21 @@ public class Main {
                     "Java Masterclass");
             System.out.println(s);
         }
+
+        Student pojoStudent = new Student("S923006", "Lin",
+                "05/11/2022", "Java Masterclass");
+        LPAStudent recordStudent = new LPAStudent("S923007", "Lilia",
+                "05/11/2025", "Java Masterclass");
+
+        System.out.println(pojoStudent);
+        System.out.println(recordStudent);
+
+        pojoStudent.setClassList(pojoStudent.getClassList() + ", Java OCP Exam 829");
+//        recordStudent.setClassList(recordStudent.classList() + ", Java OCP Exam 829");
+
+        System.out.println(pojoStudent.getName() + " is taking " +
+                pojoStudent.getClassList());
+        System.out.println(recordStudent.name() + " is taking " +
+                recordStudent.classList());
     }
 }

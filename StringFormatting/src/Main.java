2c994ce8e550f9597c1bf366617068cc4d0dffaa
khadija zekraoui <khadija.zekraoui.externe@macif.fr>
2024-09-23 16:46:50 +0200

    practise - OOP 1 - Text Block and string formatting

---
+++ StringFormatting/src/Main.java
@@ -0,0 +1,40 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        String bulletIt = "Print a Bulleted List:\n" +
+                "\t\u2022 First Point\n" +
+                "\t\t\u2022 Sub Point";
+
+        System.out.println(bulletIt);
+
+        String textBlock = """
+                
+                Print a Bulleted List:
+                    \u2022 First Point
+                        \u2022 Sub Point""";
+
+        System.out.println(textBlock);
+        System.out.println("--------------------------");
+
+        int age = 35;
+        System.out.printf("Your age is %d%n", age);
+        System.out.println("--------------------------");
+
+        int yearOfBirth = 2023 - age;
+        System.out.printf("Age = %d, Birth year = %d%n", age, yearOfBirth);
+        System.out.printf("Your age is %.2f%n", (float) age);
+        System.out.println("--------------------------");
+
+        for (int i = 1; i <= 100000; i *= 10) {
+            System.out.printf("Printing %6d %n", i);
+        }
+        System.out.println("--------------------------");
+
+        String formattedString = String.format("Your age is %d", age);
+        System.out.println(formattedString);
+
+        formattedString = "Your age is %d".formatted(age);
+        System.out.println(formattedString);
+    }
+}

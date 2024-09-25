28da4bece77f63717dbce701732f3baf86cdd662
khadija zekraoui <khadija.zekraoui.externe@macif.fr>
2024-09-25 12:57:17 +0200

    practice - OOP2 - Polymorphism Exercise

---
+++ PolymorphismExercise/src/Main.java
@@ -0,0 +1,39 @@
+public class Main {
+    public static void main(String[] args) {
+        Car car = new Car(8, "Base car");
+        System.out.println(car.startEngine());
+        System.out.println(car.accelerate());
+        System.out.println(car.brake());
+
+        Mitsubishi mitsubishi = new Mitsubishi(6, "Outlander VRX 4WD");
+        System.out.println(mitsubishi.startEngine());
+        System.out.println(mitsubishi.accelerate());
+        System.out.println(mitsubishi.brake());
+
+        Ford ford = new Ford(6, "Ford Falcon");
+        System.out.println(ford.startEngine());
+        System.out.println(ford.accelerate());
+        System.out.println(ford.brake());
+
+        Holden holden = new Holden(6, "Holden Commodore");
+        System.out.println(holden.startEngine());
+        System.out.println(holden.accelerate());
+        System.out.println(holden.brake());
+    }
+
+    /*
+     *** output:
+         * Car -> startEngine()
+         * Car -> accelerate()
+         * Car -> brake()
+         * Mitsubishi -> startEngine()
+         * Mitsubishi -> accelerate()
+         * Mitsubishi -> brake()
+         * Ford -> startEngine()
+         * Ford -> accelerate()
+         * Ford -> brake()
+         * Holden -> startEngine()
+         * Holden -> accelerate()
+         * Holden -> brake()
+    * */
+}

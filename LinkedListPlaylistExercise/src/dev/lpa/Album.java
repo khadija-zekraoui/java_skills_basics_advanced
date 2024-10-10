561ac10e69446dd1ec73e85c47b857400191f321
khadija zekraoui <khadija.zekraoui.externe@macif.fr>
2024-10-10 09:08:09 +0200

    practice - lists - LinkedList playlist exercise

--- LinkedListPlaylistExercise/src/dev/lpa/Album.java
+++ LinkedListPlaylistExercise/src/dev/lpa/Album.java
@@ -1,4 +1,59 @@
 package dev.lpa;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class Album {
+
+    private String name;
+    private String artist;
+    private ArrayList<Song> songs;
+
+    public Album(String name, String artist) {
+        this.name = name;
+        this.artist = artist;
+        this.songs = new ArrayList<>();
+    }
+
+    //ADD A SONG
+    public boolean addSong(String title, double duration) {
+
+        if (findSong(title) == null) {
+            songs.add(new Song(title, duration));
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean addToPlayList(int trackNumber, LinkedList<Song> playList) {
+
+        int index = trackNumber - 1;
+        if ((index >= 0) && (index <= songs.size())) {
+            playList.add(songs.get(index));
+            return true;
+        }
+        return false;
+    }
+
+    public boolean addToPlayList(String title, LinkedList<Song> playListSongs) {
+
+        if (findSong(title) != null) {
+            playListSongs.add(findSong(title));
+            return true;
+        }
+
+        return false;
+    }
+
+    //FIND A SONG
+    private Song findSong(String title) {
+        Song song = null;
+        for (Song listSong: songs) {
+            if (listSong.getTitle().equals(title)) {
+                song = listSong;
+            }
+        }
+        return song;
+    }
 }

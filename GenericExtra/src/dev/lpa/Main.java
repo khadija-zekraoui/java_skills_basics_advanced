831d062bb70cf25f371a4b018452d19109677b2e
khadija zekraoui <khadija.zekraoui.externe@macif.fr>
2024-10-19 18:15:29 +0200

    practice - generics - Static methods and multiple upper bounds

--- GenericExtra/src/dev/lpa/Main.java
+++ GenericExtra/src/dev/lpa/Main.java
@@ -2,9 +2,19 @@ package dev.lpa;
 
 import dev.lpa.model.LPAStudent;
 import dev.lpa.model.Student;
+import dev.lpa.util.QueryItem;
+import dev.lpa.util.QueryList;
 
 import java.util.ArrayList;
 import java.util.List;
+
+record Employee(String name) implements QueryItem {
+
+    @Override
+    public boolean matchFieldValue(String fieldName, String value) {
+        return false;
+    }
+}
 
 public class Main {
 
@@ -29,8 +39,20 @@ public class Main {
         testList(new ArrayList<String>(List.of("Able", "Barry", "Charlie")));
         testList(new ArrayList<Integer>(List.of(1, 2, 3)));
 
+        var queryList = new QueryList<>(lpaStudents);
+        var matches = queryList.getMatches(
+                "Course", "Python");
+        printMoreLists(matches);
+
+        var students2021 =
+                QueryList.getMatches(students, "YearStarted", "2021");
+        printMoreLists(students2021);
+
+//        QueryList<Employee> employeeList = new QueryList<>();
+
     }
 
+    // this is no-generic method, it uses wildcard in the type argument
     public static void printMoreLists(List<? extends Student> students) {
 
         for (var student : students) {
@@ -64,6 +86,7 @@ public class Main {
 //        }
 //    }
 
+    // This is a Generic method
 //    public static <T extends Student> void printList(List<T> students) {
 //
 //        for (var student : students) {

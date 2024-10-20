27adabada3378eeb149044a0438f3905981dd033
khadija zekraoui <khadija.zekraoui.externe@macif.fr>
2024-10-20 13:53:27 +0200

    practice - nested classes and types - inner classes challenge

--- NestedClasses/src/dev/lpa/burger/Meal.java
+++ NestedClasses/src/dev/lpa/burger/Meal.java
@@ -1,9 +1,12 @@
 package dev.lpa.burger;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Meal {
 
     private double price = 5.0;
-    private Item burger;
+    private Burger burger;
     private Item drink;
     private Item side;
 
@@ -15,7 +18,7 @@ public class Meal {
 
     public Meal(double conversionRate) {
         this.conversionRate = conversionRate;
-        burger = new Item("regular", "burger");
+        burger = new Burger("regular");
         drink = new Item("coke", "drink", 1.5);
         System.out.println(drink.name);
         side = new Item("fries", "side", 2.0);
@@ -23,14 +26,18 @@ public class Meal {
 
     public double getTotal() {
 
-        double total = burger.price + drink.price + side.price;
-        return Item.getPrice(total, conversionRate); // The name?s class Item is used to call the getPrice of the inner class
+        double total = burger.getPrice() + drink.price + side.price;
+        return Item.getPrice(total, conversionRate);
     }
 
     @Override
     public String toString() {
         return "%s%n%s%n%s%n%26s$%.2f".formatted(burger, drink, side,
                 "Total Due: ", getTotal());
+    }
+
+    public void addToppings(String... selectedToppings) {
+        burger.addToppings(selectedToppings);
     }
 
     private class Item {
@@ -40,7 +47,7 @@ public class Meal {
         private double price;
 
         public Item(String name, String type) {
-            this(name, type, type.equals("burger") ? Meal.this.price : 0); // here to use the field price of the class Meal
+            this(name, type, type.equals("burger") ? Meal.this.price : 0);
         }
 
         public Item(String name, String type, double price) {
@@ -52,12 +59,67 @@ public class Meal {
         @Override
         public String toString() {
             return "%10s%15s $%.2f".formatted(type, name,
-                    getPrice(price, conversionRate)); // here we can access the conversionRate a field of Meal class
+                    getPrice(price, conversionRate));
         }
 
         private static double getPrice(double price, double rate) {
             return price * rate;
         }
+    }
+
+    private class Burger extends Item {
+
+        private enum Extra {AVOCADO, BACON, CHEESE, KETCHUP, MAYO, MUSTARD, PICKLES;
+
+            private double getPrice() {
+                return switch (this) {
+                    case AVOCADO -> 1.0;
+                    case BACON, CHEESE -> 1.5;
+                    default -> 0;
+                };
+            }
+
+        }
+
+        private List<Item> toppings = new ArrayList<>();
+        Burger(String name) {
+            super(name, "burger", 5.0);
+        }
+
+        public double getPrice() {
+
+            double total = super.price;
+            for (Item topping : toppings) {
+                total += topping.price;
+            }
+            return total;
+        }
+
+        private void addToppings(String... selectedToppings) {
+
+            for (String selectedTopping : selectedToppings) {
+                try {
+                    Extra topping = Extra.valueOf(selectedTopping.toUpperCase());
+                    toppings.add(new Item(topping.name(), "TOPPING",
+                            topping.getPrice()));
+                } catch (IllegalArgumentException ie) {
+                    System.out.println("No topping found for " + selectedTopping);
+                }
+            }
+        }
+
+        @Override
+        public String toString() {
+
+            StringBuilder itemized = new StringBuilder(super.toString());
+            for (Item topping : toppings) {
+                itemized.append("\n");
+                itemized.append(topping);
+            }
+
+            return itemized.toString();
+        }
+
     }
 
 }

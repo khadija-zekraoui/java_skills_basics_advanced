67c7a4bdd73cbe7c510a4d5a9f704023d22a1d41
khadija zekraoui <khadija.zekraoui.externe@macif.fr>
2024-10-22 16:29:35 +0200

    practice - lambda expressions -  Functional Interfaces: Function & Supplier

--- LambdaExpressions/src/dev/lpa/Main.java
+++ LambdaExpressions/src/dev/lpa/Main.java
@@ -3,8 +3,10 @@ package dev.lpa;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 import java.util.function.BiConsumer;
 import java.util.function.BinaryOperator;
+import java.util.function.Supplier;
 
 public class Main {
 
@@ -62,6 +64,33 @@ public class Main {
         list.removeIf(s -> s.startsWith("ea"));
         list.forEach(s -> System.out.println(s));
 
+        list.replaceAll(s -> s.charAt(0) + " - " + s.toUpperCase());
+        System.out.println("-------");
+        list.forEach(s -> System.out.println(s));
+
+        String[] emptyStrings = new String[10];
+        System.out.println(Arrays.toString(emptyStrings));
+        Arrays.fill(emptyStrings,"");
+        System.out.println(Arrays.toString(emptyStrings));
+
+        Arrays.setAll(emptyStrings, (i) -> "" + (i + 1) + ". ");
+        System.out.println(Arrays.toString(emptyStrings));
+
+        Arrays.setAll(emptyStrings, (i) -> "" + (i + 1) + ". "
+            + switch (i) {
+                    case 0 -> "one";
+                    case 1 -> "two";
+                    case 2 -> "three";
+                    default -> "";
+                }
+        );
+        System.out.println(Arrays.toString(emptyStrings));
+
+        String[] names = {"Ann", "Bob", "Carol", "David", "Ed", "Fred"};
+        String[] randomList = randomlySelectedValues(15, names,
+                () -> new Random().nextInt(0, names.length));
+        System.out.println(Arrays.toString(randomList));
+
     }
 
     public static <T> T calculator(BinaryOperator<T> function, T value1, T value2) {
@@ -73,5 +102,16 @@ public class Main {
 
     public static <T> void processPoint(T t1, T t2, BiConsumer<T,T> consumer) {
         consumer.accept(t1, t2);
+    }
+
+    public static String[] randomlySelectedValues(int count,
+                                                  String[] values,
+                                                  Supplier<Integer> s) {
+
+        String[] selectedValues = new String[count];
+        for (int i = 0; i < count; i++) {
+            selectedValues[i] = values[s.get()];
+        }
+        return selectedValues;
     }
 }

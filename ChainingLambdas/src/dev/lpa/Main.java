3e7d8befd822b744ba9ffee2a956906e306fe603
khadija zekraoui <khadija.zekraoui.externe@macif.fr>
2024-10-23 17:27:01 +0200

    practice - method references - Convenience Methods, Continued, with the Comparator

--- ChainingLambdas/src/dev/lpa/Main.java
+++ ChainingLambdas/src/dev/lpa/Main.java
@@ -1,6 +1,9 @@
 package dev.lpa;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
@@ -9,11 +12,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String name = "Luna";
+        String name = "Tim";
         Function<String,String> uCase = String::toUpperCase;
         System.out.println(uCase.apply(name));
 
-        Function<String,String> lastName = s -> s.concat(" Kamar");
+        Function<String,String> lastName = s -> s.concat(" Buchalka");
         Function<String,String> uCaseLastName = uCase.andThen(lastName);
         System.out.println(uCaseLastName.apply(name));
 
@@ -21,18 +24,18 @@ public class Main {
         System.out.println(uCaseLastName.apply(name));
 
         Function<String,String[]> f0 = uCase
-                .andThen(s -> s.concat(" Kamar"))
+                .andThen(s -> s.concat(" Buchalka"))
                 .andThen(s -> s.split(" "));
         System.out.println(Arrays.toString(f0.apply(name)));
 
         Function<String,String> f1 = uCase
-                .andThen(s -> s.concat(" Kamar"))
+                .andThen(s -> s.concat(" Buchalka"))
                 .andThen(s -> s.split(" "))
                 .andThen(s -> s[1].toUpperCase() + ", " + s[0]);
         System.out.println(f1.apply(name));
 
         Function<String,Integer> f2 = uCase
-                .andThen(s -> s.concat(" Kamar"))
+                .andThen(s -> s.concat(" Buchalka"))
                 .andThen(s -> s.split(" "))
                 .andThen(s -> String.join(", ", s))
                 .andThen(String::length);
@@ -45,9 +48,9 @@ public class Main {
                 .andThen(s-> System.out.print(" - "))
                 .andThen(s1));
 
-        Predicate<String> p1 = s -> s.equals("LUNA");
-        Predicate<String> p2 = s -> s.equalsIgnoreCase("Luna");
-        Predicate<String> p3 = s -> s.startsWith("L");
+        Predicate<String> p1 = s -> s.equals("TIM");
+        Predicate<String> p2 = s -> s.equalsIgnoreCase("Tim");
+        Predicate<String> p3 = s -> s.startsWith("T");
         Predicate<String> p4 = s -> s.endsWith("e");
 
         Predicate<String> combined1 = p1.or(p2);
@@ -59,5 +62,30 @@ public class Main {
         Predicate<String> combined3 = p3.and(p4).negate();
         System.out.println("combined3 = " + combined3.test(name));
 
+        record Person(String firstName, String lastName) {}
+
+        List<Person> list = new ArrayList<>(Arrays.asList(
+                new Person("Peter", "Pan"),
+                new Person("Peter", "PumpkinEater"),
+                new Person("Minnie", "Mouse"),
+                new Person("Mickey", "Mouse")
+        ));
+
+        list.sort((o1, o2) -> o1.lastName.compareTo(o2.lastName));
+        list.forEach(System.out::println);
+
+        System.out.println("------------------------------------");
+        list.sort(Comparator.comparing(Person::lastName));
+        list.forEach(System.out::println);
+
+        System.out.println("------------------------------------");
+        list.sort(Comparator.comparing(Person::lastName)
+                        .thenComparing(Person::firstName));
+        list.forEach(System.out::println);
+
+        System.out.println("------------------------------------");
+        list.sort(Comparator.comparing(Person::lastName)
+                .thenComparing(Person::firstName).reversed());
+        list.forEach(System.out::println);
     }
 }
